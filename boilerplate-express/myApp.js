3f7b3b8411b80254f6f35cc57bba69a9468a2861
Carlos Andrés Vicioso <77629040+Vixito@users.noreply.github.com>
2025-07-20 18:08:08 +0000

    Dos solicitudes en una misma ruta

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -28,7 +28,11 @@ app.get("/now", (req, res, next) => {
 app.get("/:word/echo", (req, res) => {
     res.json({ echo: req.params.word });
 });
-
+app.route("/name").get((req, res) => {
+    res.json({ name: req.query.first + " " + req.query.last });
+}).post((req, res) => {
+    res.json({ name: req.body.first + " " + req.body.last });
+});
 
 
 
