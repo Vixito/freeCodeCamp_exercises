dd015d288308012dcb86c2d01b240e9bb97044dd
Carlos Andrés Vicioso <77629040+Vixito@users.noreply.github.com>
2025-07-21 11:34:06 +0000

    Eliminando muchos documentos

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -105,7 +105,12 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.deleteMany({ name: nameToRemove }, function(err, data) {
+    if (err) {
+      return done(err);
+    }
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
