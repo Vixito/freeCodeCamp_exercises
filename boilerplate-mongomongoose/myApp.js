c465f88a04fd54076a4fd8f7c9393c4d42772ac7
Carlos Andrés Vicioso <77629040+Vixito@users.noreply.github.com>
2025-07-21 20:06:59 +0000

    Auxiliares de consulta para búsquedas en MongoDB

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -96,7 +96,14 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select("-age")
+    .exec(function(err, data) {
+      if (err) return done(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
