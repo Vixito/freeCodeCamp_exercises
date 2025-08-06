11d05c8200030a3e8770f3ca8ddee746b6720167
Vixito <carlosandres81182@gmail.com>
2025-08-06 17:49:09 -0400

    feat: search element by atomic number, symbol, or name

--- periodic_table/element.sh
+++ periodic_table/element.sh
@@ -8,3 +8,16 @@ if [[ -z $1 ]]; then
   exit
 fi
 
+# Buscar el elemento por número, símbolo o nombre
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements 
+  JOIN properties USING(atomic_number)
+  JOIN types USING(type_id)
+  WHERE atomic_number = $1;")
+else
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements 
+  JOIN properties USING(atomic_number)
+  JOIN types USING(type_id)
+  WHERE symbol = INITCAP('$1') OR name = INITCAP('$1');")
+fi
+
