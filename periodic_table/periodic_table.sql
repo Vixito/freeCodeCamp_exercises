eb9dabc338d582b8713451db47f0db91a8bbe8e6
Vixito <carlosandres81182@gmail.com>
2025-08-06 18:19:51 -0400

    chore: Project completed!

--- periodic_table/periodic_table.sql
+++ periodic_table/periodic_table.sql
@@ -132,14 +132,14 @@ INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (8, 15.999000, -218, -183, 2);
-INSERT INTO public.properties VALUES (7, 14.007000, -210.1, -195.8, 2);
-INSERT INTO public.properties VALUES (6, 12.011000, 3550, 4027, 2);
-INSERT INTO public.properties VALUES (4, 9.012200, 1287, 2470, 1);
-INSERT INTO public.properties VALUES (3, 6.940000, 180.54, 1342, 1);
-INSERT INTO public.properties VALUES (2, 4.002600, -272.2, -269, 2);
-INSERT INTO public.properties VALUES (1, 1.008000, -259.1, -252.9, 2);
-INSERT INTO public.properties VALUES (5, 10.810000, 2075, 4000, 3);
+INSERT INTO public.properties VALUES (1, 1.008, -259.1, -252.9, 2);
+INSERT INTO public.properties VALUES (2, 4.0026, -272.2, -269, 2);
+INSERT INTO public.properties VALUES (3, 6.94, 180.54, 1342, 1);
+INSERT INTO public.properties VALUES (4, 9.0122, 1287, 2470, 1);
+INSERT INTO public.properties VALUES (5, 10.81, 2075, 4000, 3);
+INSERT INTO public.properties VALUES (6, 12.011, 3550, 4027, 2);
+INSERT INTO public.properties VALUES (7, 14.007, -210.1, -195.8, 2);
+INSERT INTO public.properties VALUES (8, 15.999, -218, -183, 2);
 INSERT INTO public.properties VALUES (9, 18.998, -220, -188.1, 2);
 INSERT INTO public.properties VALUES (10, 20.18, -248.6, -246.1, 2);
 
@@ -222,6 +222,14 @@ ALTER TABLE ONLY public.elements
 
 ALTER TABLE ONLY public.properties
     ADD CONSTRAINT fk_atomic_number FOREIGN KEY (atomic_number) REFERENCES public.elements(atomic_number);
+
+
+--
+-- Name: properties fk_type; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.properties
+    ADD CONSTRAINT fk_type FOREIGN KEY (type_id) REFERENCES public.types(type_id);
 
 
 --
