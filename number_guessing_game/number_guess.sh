8b238df78474ed01829601f599b4826787d736f9
Vixito <carlosandres81182@gmail.com>
2025-08-09 02:31:38 -0400

    feat: verify if user exists

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -5,3 +5,15 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 # pedir username
 echo "Enter your username:"
 read USERNAME
+
+# verificar si usuario existe
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+if [[ -z $USER_ID ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')" > /dev/null
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
