585a793ebad7a7140984a32b87a3fc65cc57459d
Vixito <carlosandres81182@gmail.com>
2025-08-09 02:32:12 -0400

    feat: generate secret number

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -17,3 +17,30 @@ else
   BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+# generar número secreto
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
+
+while true; do
+  read GUESS
+  ((NUMBER_OF_GUESSES++))
+
+  # validar entero
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  if (( GUESS < SECRET_NUMBER )); then
+    echo "It's higher than that, guess again:"
+  elif (( GUESS > SECRET_NUMBER )); then
+    echo "It's lower than that, guess again:"
+  else
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    $PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)" > /dev/null
+    break
+  fi
+done
